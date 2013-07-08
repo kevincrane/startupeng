386506723d8f5795d58d08b47b7eb670e3d6174c
Kevin Crane <kevincrane@gmail.com>
2013-07-08 14:09:08 -0400

    Serve index.html from root URL

--- web.js
+++ web.js
@@ -1,11 +1,19 @@
+var http = require('http'),
+    fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+// Root directory
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var out = fs.readFileSync('./index.html', 'utf8');
+    response.send(out);
 });
 
+
+// Start application
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
